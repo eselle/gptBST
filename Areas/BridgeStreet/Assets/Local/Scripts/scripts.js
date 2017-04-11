9eaa9077eafe1bd3465a45d14b0a4d5c10184434
Esteban Selle <estebans23@gmail.com>
2017-04-11 18:00:44 -0300

    Fixed location

--- Areas/BridgeStreet/Assets/Local/Scripts/scripts.js
+++ Areas/BridgeStreet/Assets/Local/Scripts/scripts.js
@@ -1931,6 +1931,8 @@ var BSGlobalDateRange = require('./BRIDGESTREET.global.search.daterange.js');
             var desktopSearchCity = document.getElementById('searchKeywords');
 
             setTimeout(function () {
+                $topFilterSearch = $('#topSearchKeywords');
+
                 var topFilterSearchCity = document.getElementById('topSearchKeywords');
                 var topFilterSearchCityAuto = new google.maps.places.Autocomplete(topFilterSearchCity, scope.cityOptions);
 
@@ -1943,7 +1945,7 @@ var BSGlobalDateRange = require('./BRIDGESTREET.global.search.daterange.js');
                     scope.lng = place.geometry.location.lng();
                     scope.place = $topFilterSearch.val();
 
-                    $topFilterSearch.val(scope.place);
+                    $mobileSearch.val(scope.place);
                     console.log('SHOW TOP FILTER');
                     BSGlobalDateRange.show();
                     return false;
